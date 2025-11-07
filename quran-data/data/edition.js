7739fa926a6e860f3e58d19e6704b010ec73dcab
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-11-07 16:06:40 +0000

    data added

---
+++ quran-data/data/edition.js
@@ -0,0 +1,325 @@
+export const editions = [
+    {
+      "identifier": "quran-wordbyword-2",
+      "language": "ar",
+      "name": "معاني مفردات القرآن ولفظها (wordbyword-2)",
+      "englishName": "Word by Word Transaltion by Dr. Shehnaz Shaikh",
+      "format": "text",
+      "type": "quran",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "quran-unicode",
+      "language": "ar",
+      "name": "القرآن الكريم برسم العثماني (unicode)",
+      "englishName": "Unicode Quran text from Khaled Hosny",
+      "format": "text",
+      "type": "quran",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "quran-uthmani-quran-academy",
+      "language": "ar",
+      "name": "القرآن الكريم برسم العثماني (quran-academy)",
+      "englishName": "Modified Quran Uthmani Text from the Quran Academy to work with the Kitab font",
+      "format": "text",
+      "type": "quran",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "ba.mehanovic",
+      "language": "ba",
+      "name": "Kur'an - sa prevodom (značenja) na bosanski jezik, utemeljen na Ibn Kesirovom tumačenju, i kratki komentar",
+      "englishName": "Quran translation by Muhamed Mehanovic",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "ru.kuliev-audio",
+      "language": "ru",
+      "name": "Elmir Kuliev by 1MuslimApp",
+      "englishName": "Elmir Kuliev by 1MuslimApp",
+      "format": "audio",
+      "type": "versebyverse",
+      "direction": null
+    },
+    {
+      "identifier": "en.itani",
+      "language": "en",
+      "name": "Clear Qur'an - Talal Itani",
+      "englishName": "Clear Qur'an by Talal Itani",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "ar.qurtubi",
+      "language": "ar",
+      "name": "تفسير القرطبي",
+      "englishName": "Tafseer Al Qurtubi",
+      "format": "text",
+      "type": "tafsir",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "ar.miqbas",
+      "language": "ar",
+      "name": "تنوير المقباس من تفسير بن عباس",
+      "englishName": "Tafseer Tanwir al-Miqbas",
+      "format": "text",
+      "type": "tafsir",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "ar.waseet",
+      "language": "ar",
+      "name": "الـتـفـسـيـر الـوسـيـط",
+      "englishName": "Tafseer Al-Waseet",
+      "format": "text",
+      "type": "tafsir",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "ar.baghawi",
+      "language": "ar",
+      "name": "تفسير البغوي",
+      "englishName": "Tafseer Al-Baghawi",
+      "format": "text",
+      "type": "tafsir",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "my.ghazi",
+      "language": "my",
+      "name": "Ghazi Muhammed Hashim",
+      "englishName": "Translation by Ghazi Muhammed Hashim",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "en.mubarakpuri",
+      "language": "en",
+      "name": "Mubarakpuri",
+      "englishName": "Mubarakpuri",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "am.sadiq",
+      "language": "am",
+      "name": "ሳዲቅ & ሳኒ ሐቢብ",
+      "englishName": "ሳዲቅ & ሳኒ ሐቢብ",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "ber.mensur",
+      "language": "ber",
+      "name": "At Mensur",
+      "englishName": "At Mensur",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "bn.hoque",
+      "language": "bn",
+      "name": "জহুরুল হক",
+      "englishName": "জহুরুল হক",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "en.qarai",
+      "language": "en",
+      "name": "Qarai",
+      "englishName": "Qarai",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "en.wahiduddin",
+      "language": "en",
+      "name": "Wahiduddin Khan",
+      "englishName": "Wahiduddin Khan",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "es.bornez",
+      "language": "es",
+      "name": "Bornez",
+      "englishName": "Bornez",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "es.garcia",
+      "language": "es",
+      "name": "Garcia",
+      "englishName": "Garcia",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "ur.najafi",
+      "language": "ur",
+      "name": "محمد حسین نجفی",
+      "englishName": "محمد حسین نجفی",
+      "format": "text",
+      "type": "translation",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "fa.gharaati",
+      "language": "fa",
+      "name": "قرائتی",
+      "englishName": "قرائتی",
+      "format": "text",
+      "type": "translation",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "fa.sadeqi",
+      "language": "fa",
+      "name": "صادقی تهرانی",
+      "englishName": "صادقی تهرانی",
+      "format": "text",
+      "type": "translation",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "fa.safavi",
+      "language": "fa",
+      "name": "صفوی",
+      "englishName": "صفوی",
+      "format": "text",
+      "type": "translation",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "id.jalalayn",
+      "language": "id",
+      "name": "Tafsir Jalalayn",
+      "englishName": "Tafsir Jalalayn",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "ml.karakunnu",
+      "language": "ml",
+      "name": "കാരകുന്ന് & എളയാവൂര്",
+      "englishName": "കാരകുന്ന് & എളയാവൂര്",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "nl.leemhuis",
+      "language": "nl",
+      "name": "Leemhuis",
+      "englishName": "Leemhuis",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "nl.siregar",
+      "language": "nl",
+      "name": "Siregar",
+      "englishName": "Siregar",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "ps.abdulwali",
+      "language": "ps",
+      "name": "عبدالولي",
+      "englishName": "عبدالولي",
+      "format": "text",
+      "type": "translation",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "ru.kuliev-alsaadi",
+      "language": "ru",
+      "name": "Кулиев + ас-Саади",
+      "englishName": "Кулиев + ас-Саади",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "ru.kuliev-audio-2",
+      "language": "ru",
+      "name": "Elmir Kuliev 2 by 1MuslimApp",
+      "englishName": "Elmir Kuliev 2 by 1MuslimApp",
+      "format": "audio",
+      "type": "versebyverse",
+      "direction": null
+    },
+    {
+      "identifier": "ce.magomedov",
+      "language": "ce",
+      "name": "Chechen by Magomedov",
+      "englishName": "Chechen by Magomedov",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "ru.transliteration",
+      "language": "ru",
+      "name": "Транскрипция",
+      "englishName": "Russian Transliteration",
+      "format": "text",
+      "type": "transliteration",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "tr.golpinarli",
+      "language": "tr",
+      "name": "Abdulbakî Gölpınarlı",
+      "englishName": "Abdulbaki Golpinarli",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },{
+      "identifier": "tr.bulac",
+      "language": "tr",
+      "name": "Alİ Bulaç",
+      "englishName": "Alİ Bulaç",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    },
+    {
+      "identifier": "quran-buck",
+      "language": "ar",
+      "name": "Buck",
+      "englishName": "Buck",
+      "format": "text",
+      "type": "translation",
+      "direction": "rtl"
+    },
+    {
+      "identifier": "zh.mazhonggang",
+      "language": "zh",
+      "name": "Ma Zhong Gang",
+      "englishName": "马仲刚",
+      "format": "text",
+      "type": "translation",
+      "direction": "ltr"
+    }
+]
